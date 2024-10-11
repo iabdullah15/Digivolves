3cfc9fdc45ff058505a3addc4f8abd80f3a74bfd
Abdullah Zubair <abdullahzubair2001@gmail.com>
2024-10-12 01:29:32 +0500

    Removed the duplicate files

--- navbar/smooth-scroll/smooth-scrolling.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const scroll = new LocomotiveScroll({
-    el: document.querySelector("[data-scroll-container]"),
-    smooth: true,
-  });
-});
