87112358d775433dee73e4fa1f6195fcb661e2f2
Abdullah Zubair <abdullahzubair2001@gmail.com>
2024-10-11 12:33:23 +0500

    Added the wave animation

---
+++ main-website/gsap-animations/wave.js
@@ -0,0 +1,7 @@
+gsap.to(".wave-wrapper", {
+    xPercent: -50, // Move it halfway left to simulate one full cycle of the wave
+    ease: "linear", 
+    duration: 10, // Adjust speed as needed for a smoother animation
+    repeat: -1 // Infinite loop
+  });
+  
