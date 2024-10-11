de6d9cb27d6315f96c900b99883d42eef40b9b82
Abdullah Zubair <abdullahzubair2001@gmail.com>
2024-10-11 06:56:22 +0500

    Added portfolio section

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,39 +1,54 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.registerPlugin(
-      Flip,
-      ScrollTrigger,
-      Observer,
-      ScrollToPlugin,
-      Draggable,
-      MotionPathPlugin,
-      EaselPlugin,
-      PixiPlugin,
-      TextPlugin,
-      RoughEase,
-      ExpoScaleEase,
-      SlowMo,
-      CustomEase
-    );
-    
-    var tl = gsap.timeline();
-  
-    // Animate the navbar container from above
-    tl.from(".navbar-container", { duration: 0.8, y: 10, opacity: 0.5 })
-    
-      // Then, animate the navbar brand separately, after the container animation
-    .from(".navbar-brand", { 
-        duration: 1, 
-        y: 10, // Start the brand slightly lower
-        opacity: 0, 
-        ease: "power2.out" // Smooth easing for a nice effect
-      }, "-=0.3") // This overlaps the brand animation to start slightly before the container finishes.
+  gsap.registerPlugin(
+    Flip,
+    ScrollTrigger,
+    Observer,
+    ScrollToPlugin,
+    Draggable,
+    MotionPathPlugin,
+    EaselPlugin,
+    PixiPlugin,
+    TextPlugin,
+    RoughEase,
+    ExpoScaleEase,
+    SlowMo,
+    CustomEase
+  );
 
-    .from(".nav-item", { 
-        duration: 1, 
-        y: 10, // Start the brand slightly lower
-        opacity: 0, 
-        ease: "power2.out" // Smooth easing for a nice effect
-        }, "-=1"); // This overlaps the brand animation to start slightly before the container finishes.
-      
-  });
-  
+  var tl = gsap.timeline();
+
+  // Animate the spans in the order you desire
+  tl.from(".animate-span", {
+    duration: 1.2,
+    y: 60,
+    opacity: 0,
+    ease: "power3.out",
+    stagger: {
+      amount: 1, // Time difference between each span animation
+      from: "end", // Start animating from the last element
+    },
+  })
+
+    // Then, start animating the navbar elements after the header text animation
+    .from(
+      ".navbar-container",
+      {
+        duration: 0.3,
+        y: 10,
+        opacity: 0.5,
+        ease: "power2.out",
+      },
+      "+=0.1"
+    ) // Delay slightly after header-text finishes
+
+    .from(
+      ".navbar-brand, .nav-item, .navbar-btn",
+      {
+        duration: 1,
+        y: 10,
+        opacity: 0,
+        ease: "power2.out",
+      },
+      "-=0.3"
+    ); // Slight overlap with previous animation
+});
