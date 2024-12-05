e485866d6da228cb104840abdbeedea623edbca4
Abdullah Zubair <abdullahzubair2001@gmail.com>
2024-12-06 02:08:12 +0500

    Fixed wave animation

--- main-website/gsap-animations/wave.js
+++ main-website/gsap-animations/wave.js
@@ -1,12 +1,3 @@
-// // GSAP Animation for seamless wave effect
-// gsap.to(".wave-wrapper", {
-//   xPercent: -50, // Move halfway to create the illusion of continuous motion
-//   duration: 10, // Adjust this for speed
-//   ease: "linear", // Linear motion for smooth and infinite animation
-//   repeat: -1, // Infinite loop
-// });
-
-// Register GSAP
 gsap.registerPlugin(ScrollTrigger);
 
 // Seamless wave animation
